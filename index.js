7bc440114d4a83c055458a444ca8c1c322d74850
Kunal0007 <73381050+Kunal0007@users.noreply.github.com>
2021-05-16 18:40:54 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-$(document).load(function () {
-  // start up after 2sec no matter what
-  document.setTimeout(function () {
-    $("body").removeClass("animate").addClass("animte");
-  }, 2000);
-});
